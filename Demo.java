d599c6cbebb588b4f78228745c309acb7f000498
Ashtami R S <ashtamirsofficial@gmail.com>
2026-03-04 23:46:48 +0530

    Practiced second largest element in an array

--- Demo.java
+++ Demo.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 class Demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev += s.charAt(i);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        if (rev.equals(s))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        int first_large = Integer.MIN_VALUE;
+        int second_large = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > first_large) {
+                second_large = first_large;
+                first_large = arr[i];
+            } else if ((arr[i] != first_large) && (arr[i] > second_large))
+                second_large = arr[i];
+        }
+        System.out.print("Second largest num: " + second_large);
     }
 }
