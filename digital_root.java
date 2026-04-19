f886f410339148281b2258370e99930daf3ceae5
Ashtami R S <ashtamirsofficial@gmail.com>
2026-04-19 15:24:36 +0530

    Add digital root calculation in Demo class
    Implement digital root calculation using iterative summation of digits.

---
+++ digital_root.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n > 9) {
+            int sum = 0;
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        System.out.println(n);
+    }
+}
